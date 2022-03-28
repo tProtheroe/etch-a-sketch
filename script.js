4368259ab4f6df8064f77b970cd7ed8b421a0237
tProtheroe <tomprothe@hotmail.co.uk>
2022-03-28 12:37:29 +0100

    Add ability to clear grid and select dimensions

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const body = document.querySelector('body')
 const grid = document.createElement('div');  
+const clearBtn = document.createElement('button');
+const body = document.querySelector('body')
 const container = document.querySelector('.container');
 
+clearBtn.textContent = "Clear Grid";
+clearBtn.id = 'clear-grid';
 grid.id = 'grid';
 
-container.appendChild(grid)
 
-grid.style.gridTemplateColumns = 'repeat(16, auto)';
-grid.style.gridTemplateRows = 'repeat(16, auto)';;
+container.appendChild(clearBtn);
+container.appendChild(grid);
 
 function makeGrid(num){
+
+    if(num > 100){
+        num = prompt("The max number is 100!");
+    }
+
+    grid.style.gridTemplateColumns = `repeat(${num}, auto)`;
+    grid.style.gridTemplateRows = `repeat(${num}, auto)`;
+
     for(let i = 1; i <= (num*num); i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
@@ -22,6 +32,21 @@ function changeColour(event){
         event.target.classList.add('hovered');
    };
 }
+
+function clearGrid(){
+    let cells = document.querySelectorAll('.cell');
+    
+    for(let cell of cells){
+        cell.classList.remove('hovered');
+        cell.remove();
+    };
+
+    let gridSize = prompt("How many squares per side do you want? (Max. 100)");
+
+    makeGrid(gridSize);
+}
+
+clearBtn.addEventListener('click', clearGrid);
 
 grid.addEventListener('mouseover', function(e){
     changeColour(e);
